b62a3c75f4249e3011a73d3a40d11db8a6ab3468
William Sl <william.sl@outlook.com>
2024-10-19 22:41:46 +0700

    test

--- IntroMinimalApiDL/obj/Debug/net8.0/IntroMinimalApiDL.AssemblyInfo.cs
+++ IntroMinimalApiDL/obj/Debug/net8.0/IntroMinimalApiDL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntroMinimalApiDL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8437fd4ddea8e14a0ad8cf6ea1fcbc58bd89597")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntroMinimalApiDL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntroMinimalApiDL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
