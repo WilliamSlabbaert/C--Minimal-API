99970cc81a3a5d519a8fce4ae9dc0e33837e647e
William Sl <william.sl@outlook.com>
2024-10-19 22:28:26 +0700

    add generic mapper

--- IntroMinimalApiBL/Models/NoteModel.cs
+++ IntroMinimalApiBL/Models/NoteModel.cs
@@ -1,8 +1,9 @@
 using System;
+using IntroMinimalApiDL;
 
 namespace IntroMinimalApiBL.Models;
 
-public class NoteModel
+public class NoteModel : IBaseModel
 {
     public int Id { get; set; }
     public string Note { get; set; }
